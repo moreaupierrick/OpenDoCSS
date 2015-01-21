29ac8b458a8d4c92cc1e2481fbc1bba8236fa71e
MIS <MikaSez@Macs.connectify>
2015-01-22 00:24:10 +0100

    [GLOBAL] Réorganisation du projet. Ajout d'un framework de test. Ajout d'un fichier d'assemblage des sources.

---
+++ openDoCSS/app.js
@@ -0,0 +1,35 @@
+
+requirejs.config({
+	baseUrl: '../lib',
+	paths: {
+		app: '../src'
+	}
+});
+
+requirejs(['app/openDoCss']);
+requirejs(['jquery.min']);
+
+
+//TODO Add conditionnal jquery.mobil.min
+
+
+/**
+WARNING :  Loading time is unknown !!!
+*/
+-(function(global){
+
+
+	var loadCss = function(url) {
+		var link = document.createElement("link");
+		link.type = "text/css";
+		link.rel = "stylesheet";
+		link.href = url;
+		document.getElementsByTagName("head")[0].appendChild(link);
+	};
+
+
+
+	loadCss("../css/openDoCSS.css");
+
+
+}());
